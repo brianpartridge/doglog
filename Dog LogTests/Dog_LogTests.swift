799d1fc48301b9acd27e78ba86d1948556c0da28
Brian Partridge <brianpartridge@squareup.com>
2016-07-09 10:28:54 -0400

    Initial commit.

---
+++ Dog LogTests/Dog_LogTests.swift
@@ -0,0 +1,36 @@
+//
+//  Dog_LogTests.swift
+//  Dog LogTests
+//
+//  Created by Brian Partridge on 7/9/16.
+//  Copyright © 2016 Pear Tree Labs. All rights reserved.
+//
+
+import XCTest
+@testable import Dog_Log
+
+class Dog_LogTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
